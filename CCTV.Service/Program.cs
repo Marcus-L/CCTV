eec48e46ea09db89dbb1f22bc1be6dfe9e1a2d83
Marcus Lum <marcusl@gmail.com>
2017-10-15 18:26:02 -0500

    windows service wrapper

---
+++ CCTV.Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using Topshelf;
+
+namespace CCTV.Service
+{
+    /// <summary>
+    /// A thin Windows Service wrapper around the .NET Core code
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<Core.Service>(s =>
+                {
+                    s.ConstructUsing(() => new Core.Service());
+                    s.WhenStarted(p => p.Start());
+                    s.WhenStopped(p => p.Stop().Wait());
+                });
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                x.EnableServiceRecovery(r =>
+                {
+                    r.RestartService(0);
+                    r.OnCrashOnly();
+                });
+
+                x.SetDescription("CCTV: A Chromecast TV Power Utility");
+                x.SetDisplayName("CCTV");
+                x.SetServiceName("CCTV");
+            });
+        }
+    }
+}
